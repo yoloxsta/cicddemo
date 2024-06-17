ebfcff2d88d17d40370f78004f1a1e039a239b11
Soe Tint Aung <123861770+yoloxsta@users.noreply.github.com>
2024-06-17 09:37:14 +0630

    push by yoloxsta

---
+++ sg.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "nginx_sg" {
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nginx_sg"
+  }
+}
